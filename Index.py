bcdf0991614b0eb19ff6b496d68af2543f659dc6
Ncsot0 <ncsotosoriano@gmail.com>
2026-03-09 23:23:52 -0400

    Suma de los elementos de la Lista

---
+++ Index.py
@@ -0,0 +1,8 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Longitud de la lista: ", len(lista))
+
+SUMA = 0
+for i in lista:
+    SUMA += i
+    
+print(f"\nSuma de los elementos de la lista: {SUMA}")
